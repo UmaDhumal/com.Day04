d771ccb8690d9746838827ead3e13aedcec06990
UmaDhumal <123440175+UmaDhumal@users.noreply.github.com>
2023-01-25 12:05:51 +0530

    by Amitji

--- GitProject02/src/com/cjc/A.java
+++ GitProject02/src/com/cjc/A.java
@@ -2,6 +2,10 @@ package com.cjc;
 
 public class A
 {
+	public void m1()
+	{
+		System.out.println("Hello World");
+	}
 	public static void main(String[] args) 
 	{
 		System.out.println("class A");
